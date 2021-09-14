b240925651663473fd588f9797798c70313f64a9
Eadwyn <zecai.zhu@legrand.com.cn>
2021-09-14 09:49:06 +0800

    spring-boot-oatuth2-demo init

---
+++ spring-boot-oatuth2-demo/src/main/java/com/taiwii/springbootoatuth2demo/SpringBootOatuth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.taiwii.springbootoatuth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOatuth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOatuth2DemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-oatuth2-demo/src/test/java/com/taiwii/springbootoatuth2demo/SpringBootOatuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwii.springbootoatuth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOatuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
